8fb97c68512ddf600808dfb07bbcfc1e833dc90f
sayali-minutus-21 <sayali.tippe@minutuscomputing.com>
2025-06-27 15:50:32 +0530

    feat: add cypress-xpath dependency and update test cases for app title verification

--- cypress/e2e/DemoSpec.cy.js
+++ cypress/e2e/DemoSpec.cy.js
@@ -3,4 +3,13 @@ describe('App Launch ', () => {
         cy.visit('https://ipaas.minutuscloud.com/dev/cg_test/')
         cy.get('h1').should('have.text', 'Motor Selector Admin Console');
     })
+    //     it('launching failed', () => {
+    //     cy.visit('https://ipaas.minutuscloud.com/dev/cg_test/')
+    //     cy.get('h1').should('have.text', 'Motor Selector Admin Panel');
+    // })
+
+    it('should display the correct title', () => {
+        cy.visit('https://ipaas.minutuscloud.com/dev/cg_test/')
+        cy.title().should('include', 'Motor Selector Admin Console');
+    })
 })
